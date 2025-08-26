81d1200af2d0fdd5a5dba5cfcb7e3d0a6afb6f4b
hpatel3-codal <hpatel3@codal.com>
2025-08-26 14:15:57 +0530

    Configure ortoni Report

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "@playwright/test";
+import { OrtoniReportConfig } from "ortoni-report";
+import * as os from "os";
 
 /**
  * Read environment variables from file.
@@ -11,18 +13,42 @@ import { defineConfig } from "@playwright/test";
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+const reportConfig: OrtoniReportConfig = {
+  open: process.env.CI ? "never" : "always", // default to never
+  folderPath: "report-db",
+  filename: "index.html",
+  title: "Codal Test Report",
+  showProject: !true,
+  projectName: "Ortoni-Report",
+  testType: "Functional",
+  authorName: os.userInfo().username,
+  base64Image: false,
+  stdIO: false,
+  preferredTheme: "light",
+  chartType: "doughnut", //doughnut | pie
+  meta: {
+    project: "Playwright",
+    version: "3",
+    description: "Playwright test report",
+    testCycle: "04121994",
+    release: "0.3",
+    platform: os.type(),
+  },
+};
+
+
 export default defineConfig({
   testDir: "./tests",
   testMatch: ["**/*.spec.ts", "**/*.test.ts"],
   /* Run tests in files in parallel */
   fullyParallel: true,
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: [[`html`, { outputFolder: 'html-report', open: 'never' }], ['ortoni-report', reportConfig]],
   use: {
     baseURL: "https://www.saucedemo.com",
     trace: "on-first-retry",
     screenshot: "only-on-failure",
-    video: "retain-on-failure",
+    video: "on",
     browserName: "chromium",
     headless: false,
   },
